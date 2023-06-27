b62e30d9412e1494c67446de41e0b135f9806ac6
Naveedullah <naveedullah968@gmial.com>
2023-06-27 08:40:51 -0700

    quick fixes

--- src/components/pages/signup/Signup.js
+++ src/components/pages/signup/Signup.js
@@ -5,8 +5,11 @@ import Alert from '../../Alerts/alert';
 
 function Signup() {
     const [user, setUser]=useState('');
+    const [category, setCategory] = useState([]);
     const [alert, setAlert] = useState(null);
-    const url='createUser';
+    
+    const url='http://localhost/khadim-hazir/api/createUser';
+
 
     const showAlert = (message, type) => {
       setAlert({
@@ -26,14 +29,27 @@ function Signup() {
         [e.target.name]: value
       });
     };
-  
+
+    const category_url='';
+        useEffect(() => {
+            axios.get(`${category_url}`).then(response => {
+            setCategory(response?.data?.data?.en);
+            }).catch(error => {
+            console.error(error);
+          });
+          
+       }, []
+       
+       
+       );
+
     const handleSubmit = (e) => {
       e.preventDefault();
           const formData = new FormData()
           formData.append('username',  user.username,)
           formData.append('email',  user.email,)
           formData.append('password',  user.password,)
-          formData.append('user_type',  user.employer,)
+          formData.append('user_type',  user.usertype,)
           formData.append('phone_no',  user.phone_no,)
           formData.append('address',  user.address,)
           formData.append('image',  user.image,)
@@ -51,6 +67,7 @@ function Signup() {
           .catch(error => {
           console.error(error);
           });
+
     };
   
     function SubmitButton(){
@@ -60,72 +77,43 @@ function Signup() {
         return <button className="btn login-btn"  onClick={handleSubmit} type="submit" disabled>Sign Up</button>
       };
     };
-  
+
     return <section className="login-section pl-2">    
     <div className="container">
       <div className="row login-form ">
         <div className="col-md-8">
-          <form action="">
+        <Alert alert={alert}/>
+          <form action="" >
             <h2 className="text-center pt-4">Create Account</h2>
             <div className="social-media-links d-flex justify-content-center pt-3">
               <Link to=""><i class="fa-brands fa-facebook"></i></Link>
               <Link href=""><i class="fa-brands fa-linkedin"></i></Link>
               <Link href=""><i class="fa-brands fa-google-plus"></i></Link>
             </div>
-            <p className="text-center">or use your email for registeration</p>
-            <Link href=""><img src="assets/images/images (5).jpg" alt="" class="profile"/></Link>
-            
+            <p className="text-center">Or use your email for registeration</p>
             <div class="container pt-3">
               
     <div class="row justify-content-center">
       <div class="col-auto">
-      
-        <div class="form-check form-check-inline pl-5">
-          <input class="form-check-input m-4  " type="radio" name="radio-group" id="radio1" value="employee"/>
+      <input class="form-check-input m-4  " type="radio" name="usertype" id="radio1" value="employee" onChange={handleChange}/>
+        <div class="form-check form-check-inline">
           <div class="row .redio-buttons-image">
-          <Link href=""><img src="assets/images/labour.png" alt="" class=" labour"/></Link>
-          <label>
-            <input
-              type="radio"
-              value="employer"
-              // checked={this.state.selectedOption === "Male"}
-              // onChange={this.onValueChange}
-            />
-            Labour
-          </label>
+          <Link><img src="assets/images/labour.png" alt="" class=" labour"/></Link>
+          <label>Labour</label>
           </div>
         </div>
-        
+        <input class="form-check-input m-4" type="radio" name="usertype" id="radio2" value="employer" onChange={handleChange}/>
         <div class="form-check form-check-inline pr-5">
-          <input class="form-check-input m-4" type="radio" name="radio-group" id="radio2" value="employer"/>
+         
           <div class="row .redio-buttons-image">
-          <Link href=""><img src="assets/images/employee.png" alt="" class=" labour"/></Link>
-          <label>
-            <input
-              type="radio"
-              value="em[ployee"
-              // checked={this.state.selectedOption === "employer"}
-              // onChange={this.onValueChange}
-            />
-            Employer
-          </label>
+          <Link ><img src="assets/images/employee.png" alt="" class=" labour"/></Link>
+          <label>Employer</label>
           </div>
-          
         </div>
-    
     </div>
     </div>
   </div>
-            <div class="form-group">
-            <div className="col-md-8 mb-4">
-            <select id="signup-sector" name="signup-sector" class="signup-select">
-                        <option value="sector">Sector</option>
-                        <option value="volvo">Volvo</option>
-                        <option value="saab">Saab</option>
-                        <option value="mercedes">Mercedes</option>
-                    </select>
-                    </div>
-          </div>
+           
             <div className="name-input mb-4 d-flex">
               <label for=""><i className="fas fa-user"></i></label>
               <input className="" type="name" name='username' placeholder="Name" value={user.username} onChange={handleChange}/>
@@ -134,27 +122,45 @@ function Signup() {
               <label for=""><i className="far fa-envelope"></i></label>
               <input className="" type="email" name='email' placeholder="Email" value={user.email} onChange={handleChange}/>
             </div>
+            <div className="password-input mb-4 d-flex">
+              <label for=""><i className="fas fa-lock	"></i></label>
+              <input className="password-input" type="password" name="password" id="" placeholder="Password" value={user.password} onChange={handleChange}/>
+            </div>
             <div className="name-input mb-4 d-flex">
               <label for=""><i className="fas fa-phone"></i></label>
               <input className="" type="number" name='phone_no' placeholder="Phone number" value={user.phone_no} onChange={handleChange}/>
             </div>
-            <div className="password-input d-flex">
-              <label for=""><i className="fas fa-lock	"></i></label>
-              <input className="password-input" type="password" name="password" id="" placeholder="Password" value={user.password} onChange={handleChange}/>
-            </div>
           <div className="row">
-              <div className="text-left">
+              <div className="text-left mb-4">
                 <label className="text-left comment" for="floatingTextarea2">Write your address here</label>
                  <textarea className="form-control" name="address" id="floatingTextarea2" value={user.address} onChange={handleChange}
             ></textarea>
               </div>              
             </div>
+            {(() => {
+        if (user.usertype === "employee") {
+          return (
+            <div class="form-group">
+            <div className="col-md-8 mb-4">
+            <select id="signup-sector" name="signup-sector" class="signup-select">
+            {category.map(categories => ( <option>{categories.name}</option>))}
+            </select>
+            </div>
+          </div>
+          )
+        } 
+      })()}
+           <div className="name-input d-flex">
+              <label for=""><i className="fas fa-user"></i></label>
+              <input className="" type="file" name='image' placeholder="Upload image" value={user.image} onChange={handleChange}/>
+            </div>
             <div className="row">
-              <div className="col-6 text-left mt-2">
+              <div className="col-6 text-left">
                 <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
                 <label for="vehicle1">Remember me</label>
               </div>
             </div>
+            
             <SubmitButton/>
           </form>
         </div>
@@ -163,8 +169,8 @@ function Signup() {
         <div className="col-md-4 pr-0">
           <div className="details">
             <h3 className="text-light">Welcome</h3>
-            <p className="text-light mb-5">to keep connected with us please sign-up with your personal info </p>
-            <Link className="text-light" to="/login">already have an Account<i className="ml-3 fas fa-arrow-right"></i></Link>
+            <p className="text-light mb-5">To keep connected with us please sign-up with your personal informations </p>
+            <Link className="text-light" to="/login">Already have an Account<i className="ml-3 fas fa-arrow-right"></i></Link>
           </div>
         </div>
       </div>
