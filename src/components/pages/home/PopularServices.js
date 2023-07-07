b54ff22c4fcb18323c155cf4864ff9cea015598b
Naveedullah <naveedullah968@gmial.com>
2023-07-07 07:53:34 -0700

    quick fixes

--- src/components/pages/home/PopularServices.js
+++ src/components/pages/home/PopularServices.js
@@ -1,7 +1,87 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
+import axios from '../../../config/axios';
 
 function PopularServices() {
+  const [category, setCategory] = useState([]);
+  const category_url='popularCategories';
+        useEffect(() => {
+            axios.get(`${category_url}`).then(response => {
+            setCategory(response?.data?.data?.en);
+            }).catch(error => {
+            console.error(error);
+          }); 
+          
+       }, []
+       );
+       const [currentPage, setCurrenPage] = useState(1);
+       const numberofcategories= 5;
+        const lastIndex = currentPage * numberofcategories;
+        const firstIndex = lastIndex - numberofcategories;
+
+       const records = category.slice(firstIndex, lastIndex);
+
+  // const [currentSlide, setCurrentSlide] = useState(0);
+  
+const handlePrevClick = () => {
+  setCurrenPage((prevSlide) => (prevSlide === 0 ? records.length - 1 : prevSlide - 1));
+};
+
+const handleNextClick = () => {
+  setCurrenPage((prevSlide) => (prevSlide === records.length - 1 ? 0 : prevSlide + 1));
+};
+
+
+
+  return <section className="pt-5 pb-5 worker-carousel mb-5">
+     <div className="container">
+      <div className="row">
+        <div className="col-md-6 mb-5">
+          <h2 className="mb-3 categories">Popular Services </h2>
+        </div>
+        <div className="container categories">
+          <div className="col-md-12">
+            <div id="carouselExampleIndicators2" className="carousel slide" data-ride="carousel">
+              <div className="carousel-inner">
+                <div className="carousel-item active">
+                  <div className="row">
+                    <div className="col-lg-1 pt-5">
+                      
+                      <a className="btn carousel-btn-left mb-3 mr-1" href="#carouselExampleIndicators2" role="button" data-slide="prev">
+                        <i className="fa fa-arrow-left"></i>
+                      </a>
+                    </div>
+                    {records.map(categories => (
+                    <div className="col-md-2 mb-3">
+                      <Link to={`/labours/${categories.slug}`}>
+                      <div className="carousel-card text-center">
+                        <img className="img-fluid mx-auto" src={`${process.env.REACT_APP_RESOURCES_URL}uploads/category/${categories.image}`} alt=""/>
+                        <div className="card-body">
+                        <p>{categories.name}</p>
+                        </div>
+                      </div>
+                      </Link>
+                    </div>
+                  
+                    ))}
+                    <div className="col-lg-1 pt-5">
+                      <a className="btn carousel-btn-right mb-3 " href="#carouselExampleIndicators2" role="button" data-slide="next">
+                        <i className="fa fa-arrow-right"></i>
+                      </a>
+                    </div>
+                  </div>
+                </div>          
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+  </section>; 
+}
+
+function PopularServices1() {
   return <section className="pt-5 pb-5 worker-carousel mb-5">
      <div className="container">
       <div className="row">
--- src/components/pages/home/Search.js
+++ src/components/pages/home/Search.js
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import axios from '../../../config/axios';
+import { BrowserRouter as Router,Routes, Route, Link, useParams  } from 'react-router-dom';
+
 function Search() {
     return <><div className="container pt-4">
     <div className="row">
@@ -14,66 +18,76 @@ function Search() {
     <div className="row">
       <div className="col-lg-2 ">
         <div className="card border-0 ">
+          <Link to='/labours/handy-person'>
           <div className="mx-auto">
             <img src="assets/images/svgviewer-png-output.png" className="card-img-top  mx-auto " alt="..."/>
             <p className="card-title text-center services pt-3 ">Handy person</p>
-
           </div>
-
+          </Link>
         </div>
       </div>
       <div className="col-lg-2 ">
         <div className="card border-0 ">
+          <Link to='/labours/landscaping'>
           <div className="mx-auto">
             <img src="assets/images/svgviewer-png-output (1).png" className="card-img-top  mx-auto " alt="..."/>
           </div>
           <div className="card-body">
             <p className="card-title text-center services ">Landscaping</p>
-
           </div>
+          </Link>
         </div>
       </div>
+      
       <div className="col-lg-2 ">
         <div className="card border-0 ">
-          <div className="mx-auto">
-            <img src="assets/images/svgviewer-png-output (2).png" className="card-img-top  mx-auto " alt="..." />
+        <Link to='/labours/plumbing-services'> 
+        <div className="mx-auto">
+          <img src="assets/images/svgviewer-png-output (2).png" className="card-img-top  mx-auto " alt="..." />
           </div>
           <div className="card-body">
-            <p className="card-title text-center services ">Plumbing</p>
-
+           <p className="card-title text-center services ">Plumbing</p>
           </div>
+          </Link>
         </div>
       </div>
+      
       <div className="col-lg-2 ">
         <div className="card border-0 ">
+        <Link to='/labours/elctrician'>
           <div className="mx-auto">
             <img src="assets/images/svgviewer-png-output (3).png" className="card-img-top  mx-auto " alt="..." />
           </div>
           <div className="card-body">
             <p className="card-title text-center services ">Electrician</p>
-
           </div>
+          </Link>
         </div>
       </div>
       <div className="col-lg-2">
         <div className="card border-0">
+        <Link to='/labours/remodling'>
           <div className="mx-auto">
             <img src="assets/images/svgviewer-png-output.png" className="card-img-top  mx-auto " alt="..." />
           </div>
           <div className="card-body">
             <p className="card-title text-center services ">Remodeling</p>
           </div>
+          </Link>
         </div>
       </div>
       <div className="col-lg-2">
         <div className="card border-0">
+        <Link to='/labours/roof-work'>
           <div className="mx-auto">
             <img src="assets/images/svgviewer-png-output (5).png" className="card-img-top  mx-auto" alt="..." />
           </div>
           <div className="card-body">
             <p className="card-title text-center services ">Roofing</p>
           </div>
+          </Link>
         </div>
+        
       </div>
     </div>
   </div>
