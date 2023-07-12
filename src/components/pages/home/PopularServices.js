d1d3e23725ac0bc683aa98c31d939262f429388c
asimsajjad <46066595+asimsajjad@users.noreply.github.com>
2023-07-12 18:26:20 +0500

    html-fixes

--- src/components/pages/home/PopularServices.js
+++ src/components/pages/home/PopularServices.js
@@ -16,25 +16,53 @@ function PopularServices() {
       
    }, []
    );
+   
     var settings = {
-        dots: false,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 5,
-        slidesToScroll: 1
-      }; 
+      dots: false,
+      infinite: false,
+      speed: 500,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
   return <section className="container-fluid services">
     <div className='container'>
     <div className="col-md-6 mb-5">
           <h2 className="mb-3 categories">Popular Services </h2>
         </div>
     </div>
-  <div className="container mx-auto slid">
-      <div className="row slider" >
+  <div className="container mx-auto ">
+      <div className="row slider mx-auto " >
       <Slider {...settings}>
 
       {categories.map(category => (
-                        <div className="col-md-2 mb-3 gridbox">
+                        <div className="col-md-2">
                             <Link to={`labours/${category.slug}`}>
                                 <div className="carousel-card text-center">
                                     <img className="img-fluid mx-auto rounded mx-auto d-block"
