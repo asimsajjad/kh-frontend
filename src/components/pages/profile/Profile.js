25035104cd2f588dec8ba157a3032c0daaa395aa
asimsajjad <46066595+asimsajjad@users.noreply.github.com>
2023-07-18 18:19:18 +0500

    html-fixes

--- src/components/pages/profile/Profile.js
+++ src/components/pages/profile/Profile.js
@@ -40,33 +40,70 @@ function Info() {
        const renderUser=(
            <div>
            {profile.map(info => (
-        <div className="container pt-5">
+        <div className="container pt-5 profile">
                     <div className="row pl-2">
-                        <div className="col-lg-3">
+                        <div className="col-lg-3 p-5">
                         <img src={info.image ? `${process.env.REACT_APP_RESOURCES_URL}images/${info.image}` : `${process.env.REACT_APP_BASE_URL}assets/images/manager.png`} className="img-fluid profile" />
-                        </div>
                         <div className="col-lg-2 pt-5">
-                            <p className="p1">{info.username}</p>
+                        </div>
+                        </div>
+                        <div className="col-lg-5 pt-3">
+                        <div className="col-md-12 ">
+                                 <h1>About</h1>
+                         <div className="row pt-3">
+                         <div className="col-md-4 ">
+                          <p className="p1">{info.username}</p>
+                        </div>
+            <div className="col-md-8 ">
+            <p className='p1'> <img src={`${process.env.REACT_APP_BASE_URL}assets/images/location.png`} className='location_icon '></img>Pakistan</p> 
+            </div>
+            </div>
                             <p className="p2">{info.category_name}</p>
                             {(() => {
                                 if (user_id == info.user_id){
                                     return (<p></p>)
                                 }else{
                                     return (
-                                    <div className="col-lg-6 pt-3">
+                                    <div className="col-lg-6">
                                     <button type="button" className={isActive ? 'btn btn-success-hide' : 'btn btn-success'} style={isActive ? {display : 'none'} : {display : 'block'}}  onClick={handleClick}>Call Now</button>
                                     {description && (<button type="button" style={{background:'none', border:'none'}} onClick={handleButton}><p><b>{info.phone_no}</b></p></button>)}
-                                    </div>)}              
+                                    </div>)}  
+                                                
                             })()}
-                        </div>
-                        <div className="col-lg-2 pt-5">
-                        <p className='p1'> <img src={`${process.env.REACT_APP_BASE_URL}assets/images/location.png`} className='location_icon '></img>Pakistan</p>   
-                       </div>  
-                    <div className="col-lg-4 profile-text  pt-3">
-                        this 
-                    </div>
+        </div>
+        <div className="col">
+            <div className="row pt-3">
+                <div className="col-md-3 ">
+                    <p className="p4">Address:</p>
                 </div>
-               
+                <div className="col-md-9">
+                    <p className="p4">{info.address}</p>
+                </div>
+            </div>
+        </div>
+        <div className="col">
+            <div className="row ">
+                <div className="col-md-3">
+                    <p className="p4">Email:</p>
+                </div>
+                <div className="col-md-3 ml-5">
+                    <p className="p4">{info.email}</p>
+                </div>
+            </div>
+        </div>
+                        <div className="row">
+                <div className="col-md-3">
+                    <p className="p4">Phone:</p>
+                </div>
+                <div className="col-md-3 ml-5">
+                    <p className="p4">{info.phone_no}</p>
+                </div>
+            </div>
+                       </div>  
+                    <div className="col-lg-4 profile-text  pt-5 text-center text-white">
+                       <h3>Welcome to khadim hazir</h3> 
+                    </div>
+                </div> 
         {/* <div className="col-12 pt-5">
             <h1>Description</h1>
         </div>
@@ -80,42 +117,9 @@ function Info() {
                 elementum tellus.
             </p>
         </div> */}
-        <div className="col-md-12 pt-5">
-            <h1>About</h1>
-        </div>
-        <div className="col-md-12 pt-5">
-            <div className="row">
-                <div className="col-md-3">
-                    <p className="p4">Phone:</p>
-                </div>
-                <div className="col-md-3">
-                    <p className="p4">{info.phone_no}</p>
-                </div>
-            </div>
-    
-        </div>
-        <div className="col">
-            <div className="row pt-3">
-                <div className="col-md-3 ">
-                    <p className="p4">Address:</p>
-                </div>
-                <div className="col-md-9">
-                    <p className="p4">{info.address}</p>
-                </div>
-            </div>
-    
-        </div>
-        <div className="col">
-            <div className="row pt-3">
-                <div className="col-md-3">
-                    <p className="p4">Email:</p>
-                </div>
-                <div className="col-md-3">
-                    <p className="p4">{info.email}</p>
-                </div>
-            </div>
-    
-        </div>
+        
+     
+        
     </div>
      ))}
      </div>);
